cefaea85b7443af21523b1041b44f555d3b57b61
Jared Moore <jared.moore@intelliapps.io>
2018-09-22 16:08:21 -0400

    edits

--- Ballz/sketch/Rect.ts
+++ Ballz/sketch/Rect.ts
@@ -1,6 +1,7 @@
 class Rect {
   width: number;
   height: number;
+  floating: boolean = true;
   body: Matter.Body;
 
   constructor(x: number, y: number, width: number, height: number, options?: Matter.IChamferableBodyDefinition) {
@@ -14,16 +15,41 @@ class Rect {
     Matter.World.remove(world, this.body);
   }
 
+  setPosition(x: number, y: number) {
+    if (this.floating) {
+      const options: Matter.IChamferableBodyDefinition = {
+        position: {x, y}
+      };
+      Matter.Body.set(this.body, options);
+    }
+  }
+
+  drop() {
+    this.floating = false;
+    const options: Matter.IChamferableBodyDefinition = {
+      isStatic: false
+    };
+    Matter.Body.set(this.body, options);
+  }
+
+
   draw(p: p5) {
-    const posX = this.body.position.x;
-    const posY = this.body.position.y;
-
-    p.push();
-
-    p.translate(posX, posY);
-    p.rotate(this.body.angle);
-    p.rect(0, 0, this.width, this.height);
-
-    p.pop();
+    p.beginShape();
+    this.body.vertices.forEach(v => p.vertex(v.x, v.y)); 
+    p.endShape(p.CLOSE);
   }
 }
+
+// draw(p: p5) {
+//   const posX = this.body.position.x;
+//   const posY = this.body.position.y;
+//   const vertices = this.body.vertices;
+
+//   p.push();
+
+//   p.translate(posX, posY);
+//   p.rotate(this.body.angle);
+//   p.rect(0, 0, this.width, this.height);
+
+//   p.pop();
+// }
