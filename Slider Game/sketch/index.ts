109ab3bf21e753bf33f618da94817918bd89a324
Jared Moore <jared.moore@intelliapps.io>
2018-09-22 20:29:46 -0400

    Game Finished

--- Slider Game/sketch/index.ts
+++ Slider Game/sketch/index.ts
@@ -1,54 +1,90 @@
 class Sketch {
-  blocks: Array<Block> = [];
-  loopDegree: number = 0;
-  allowDrop: boolean = true;
-  height: number = 20;
-  width: number = 300;
-  level: number = 1;
+  allowTouch: boolean;
+  blocks: Array<Block>;
+  loopDegree: number;
+  gameOver: boolean;
+  height: number;
+  width: number;
+  level: number;
   levelHeight = (p: p5) => p.height - (this.height * (this.level + 1));
 
-  private slideCurrentLevel(p: p5) { 
-    const block = this.blocks[this.blocks.length - 1];
-    const x = p.map(p.sin(this.loopDegree), -1, 1, -1 * (p.width / 2), (p.width / 2) - block.width);
-    block.setPosition(x, block.position.y); 
-    if (this.loopDegree > 358) { this.loopDegree = 0 } else { this.loopDegree ++ }
+  private initialize(p: p5) {
+    this.allowTouch = true;
+    this.blocks = [];
+    this.loopDegree = p.random(0, 360);
+    this.gameOver = false;
+    this.height = 40;
+    this.width = p.width < 600 ? p.width - 200 : 400;
+    this.level = 1;
+    this.blocks.push(new Block(0 - this.width / 2, p.height - this.height, this.width, this.height, p)); // initialize level 0
+    this.blocks.push(new Block(0, this.levelHeight(p), this.width, this.height, p)); // initialize level 1
   }
 
-  private onPlaceBlock(p: p5) {
-    p.keyPressed = () => { 
-      if (p.keyCode === 32 ) {
-        this.nextLevel(p);
-      }
+  private slideCurrentLevel(p: p5) {
+    const block = this.blocks[this.blocks.length - 1];
+    const bound = {
+      max: 600,
+      left: () => -1 * (p.width / 2) > -1 * bound.max ? -1 * (p.width / 2) : -1 * bound.max,
+      right: () => (p.width / 2) < bound.max ? (p.width / 2) - block.width : bound.max - block.width
     }
-    // if (p.keyIsPressed && p.keyCode === 32) {
-    //   this.nextLevel(p);
-    // }
+    const x = p.map(p.sin(this.loopDegree), -1, 1, bound.left(), bound.right());
+    block.setPosition(x, block.position.y);
+    if (this.loopDegree > 360) { this.loopDegree = 0 } else { this.loopDegree += 0.5 + (this.level / 10) }
   }
-  
+
+  private onInput(func: Function, p: p5) {
+    p.keyPressed = () => p.keyCode === 32 ? func() : null;
+    p.touchStarted = () => { if (this.allowTouch) func(); this.allowTouch = false; }
+    p.touchEnded = () => setTimeout(() => this.allowTouch = true, 500);
+  }
+
   private nextLevel(p: p5) {
-    this.level ++; 
+    this.level++;
     const blockA = this.blocks[this.blocks.length - 1];
     const blockB = this.blocks[this.blocks.length - 2];
-    const newBlock = new Block(0, this.levelHeight(p) , this.width, this.height, p);
-    this.blocks.push(newBlock);
+    let difference = blockB.position.x - blockA.position.x;
+    const newWidth = Math.abs(this.width - Math.abs(difference));
+    if (newWidth <= this.width) {
+      // Check if last block was not stacked
+      let boundaryA = {start: blockA.position.x + (p.width / 4), end: 0}; boundaryA.end = boundaryA.start + blockA.width;
+      let boundaryB = {start: blockB.position.x + (p.width / 4), end: 0}; boundaryB.end = boundaryB.start + blockB.width;
+      if (boundaryA.end < boundaryB.start || boundaryA.start > boundaryB.end ) { this.gameOver = true }
+      
+      // Shrink next level width
+      if (Math.abs(difference) < 20) { difference = 0 } else { this.width = newWidth; blockA.width = this.width; }
+      if (difference >= 0) blockA.setPosition(blockB.position.x, blockA.position.y); // Move left on left-sided hangover
+      
+      // Next level
+      const newBlock = new Block(0, this.levelHeight(p), this.width, this.height, p);
+      this.blocks.push(newBlock);
+    } else {
+      this.gameOver = true;
+    }
   }
 
   public setup(p: p5) {
+    p.pixelDensity(1);
     p.angleMode(p.DEGREES);
-    p.createCanvas(p.windowWidth, p.windowHeight);  
-    this.blocks.push(new Block(0 - this.width / 2, p.height - this.height, this.width, this.height, p)); // initialize level 0
-    this.blocks.push(new Block(0, this.levelHeight(p), this.width, this.height, p)); // initialize level 1
- }
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    this.initialize(p);
+  }
 
   public draw(p: p5) {
-    p.background(100);
-    p.translate(p.width / 2, 0); // center x-axis
-    if (this.levelHeight(p) < p.height / 2) p.translate(0, (-1 * this.levelHeight(p)) + p.height / 2); // shift scene down when blocks stack too high
-    
-    this.slideCurrentLevel(p);
-    this.onPlaceBlock(p);
-    this.blocks.forEach(block => block.draw(p)); // render blocks
-
+    if (!this.gameOver) {
+      p.background(100);
+      p.translate(p.width / 2, 0); // center x-axis
+      if (this.levelHeight(p) < p.height / 2) p.translate(0, (-1 * this.levelHeight(p)) + Math.floor(p.height / 2)); // shift scene down when blocks stack too high
+      this.slideCurrentLevel(p);
+      this.onInput(() => this.nextLevel(p), p);
+      this.blocks.forEach(block => block.draw(p)); // render blocks
+    } else {
+      p.background(p.color(255, 0, 0));
+      p.translate((p.width / 2) - 100, p.height / 2); // center x-axis
+      p.fill(255, 255, 255);
+      p.textSize(32);
+      p.text("Game Over!", 0, 0);
+      this.onInput(() => this.initialize(p), p);
+    }
   }
 }
 
